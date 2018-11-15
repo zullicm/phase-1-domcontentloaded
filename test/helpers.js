cb92a39e189daa274a12a137f2abbb79a860fc9c
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-15 11:24:10 -0500

    Adds tests (not fully working)

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,6 @@
 const chai = require('chai')
+const sinon = require('sinon');
+global.Should = require('chai').Should()
 global.expect = chai.expect
 const fs = require('file-system')
 const jsdom = require('mocha-jsdom')
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,15 @@
+const sinon = require('sinon');
+
 describe("index.js", () => {
-  it("adds event listener to document", () => {
+  it('has an eventlistener', function() {
+    sinon.spy(document, 'addEventListener')
+  
+    domContentLoaded()
     expect(document.addEventListener.calledOnce).to.be.true
   })
+  it('should change the text on the page', function(){
+    expect(document.querySelector('p')).should.have.text("JavaScript is so cool. It lets me add text to my page programmatically.");
+    domContentLoaded()
+    expect(document.querySelector('p')).should.have.text("This is really cool!");
+  });
 })
