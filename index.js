a7f8f3994d68a818fcbebde4c424a3c0980f2f2a
maxwellbenton <maxwellbenton@gmail.com>
2018-11-19 08:09:18 -0500

    added solution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
+document.addEventListener( "DOMContentLoaded", updateDOM );
 
-function testFunc() {
-  return "hi"
+function updateDOM() {
+  document.getElementById( "text" ).innerHTML = "This is really cool!";
 }
