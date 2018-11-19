cf5559a81a6b5f711b941669f885b70456aeebe7
maxwellbenton <maxwellbenton@gmail.com>
2018-11-19 08:09:10 -0500

    updated test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,11 @@
-describe("index.js", () => {
-  it("adds event listener to document", () => {
-    expect(document.addEventListener.calledOnce).to.be.true
-  })
-})
+const fs = require( 'file-system' )
+const path = require( 'path' )
+
+describe( "index.js", () => {
+  it( "adds event listener to document", async () => {
+    const js = await fs.readFileSync( path.resolve( __dirname, '..', 'index.js' ), 'utf-8' )
+
+    expect( js ).to.match( /document.addEventListener\(( |)"DOMContentLoaded"[\S\s]*updateDOM[\S\s]*/ )
+
+  } )
+} )
